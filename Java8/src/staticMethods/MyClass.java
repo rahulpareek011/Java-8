925317210656996264665717e2aeb69e6f166e77
rahul pareek <itsrahul615@gmail.com>
2026-01-27 17:46:40 +0530

    allow static main method in java 8

---
+++ Java8/src/staticMethods/MyClass.java
@@ -0,0 +1,7 @@
+package staticMethods;
+
+public class MyClass {
+	public static void main(String[] args) {
+		System.out.println("Hello !");
+	}
+}
---
+++ Java8/src/staticMethods/MyInterface.java
@@ -0,0 +1,7 @@
+package staticMethods;
+
+public interface MyInterface {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
